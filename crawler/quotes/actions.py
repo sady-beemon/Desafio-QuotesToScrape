e001fc4376118ea7dfb931d1dfacd23c5c8ca1f1
Sady <sady.costamilan@beemon.com.br>
2024-03-27 15:43:36 -0300

    Finalizaçao do Quotes To Scrape

--- crawler/quotes/actions.py
+++ crawler/quotes/actions.py
@@ -8,22 +8,8 @@ def data_request():
     soup = BeautifulSoup(r.text,"html.parser")
     tags = soup.find_all(class_ = "quote")
     for tag in tags:
-        # Crie um dicionario com cada tag
-        # Crie um Quote com o dict (dica pode passar como **kwargs)
         Quotes.objects.create(
             content = (tag.find(class_ = "text")).get_text(),
             creator = (tag.find(class_ = "author")).get_text(),
             tags = (tag.find(class_ = "tags")).get_text(),
-            
         )
-
-def single_request():
-    r = requests.get('https://quotes.toscrape.com/')
-    soup = BeautifulSoup(r.text,"html.parser")
-    Quotes.objects.create(
-        content = (soup.find(class_ = "text")).get_text(),
-        creator = (soup.find(class_ = "author")).get_text(),
-        tags = (soup.find(class_ = "tags")).get_text(),
-    
-        
-    )
--- crawler/quotes/admin.py
+++ crawler/quotes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from quotes.models import Quotes
-from quotes.actions import data_request, single_request
+from quotes.actions import data_request
 
 
 
@@ -12,10 +12,6 @@ class QuotesAdmin(admin.ModelAdmin):
     @admin.action(description="Fill in with data from https://quotes.toscrape.com/")
     def fill_in(modeladmin, request, queryset):
             data_request()
-
-    #@admin.action(description="WIP - Fill selected with data from https://quotes.toscrape.com/")
-    #def fill_in_selected(modeladmin, request, queryset):
-            #single_request()
 
 admin.site.register(Quotes, QuotesAdmin)
 
